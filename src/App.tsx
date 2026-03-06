26831d20c5b6d71a36ed35c680e18d31c8977eb5
Josef8852 <josefaboshady2@gmail.com>
2026-03-06 01:24:55 +0100

    install StyledComponents & change Font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+
 
 
 const App:React.FC = () => {
   
   return (
-    <div>
-      
-    </div>
+    <>
+      <GlobalStyles/>
+    </>
   )
   
   
