73cf4d08a4c8849efeeb0a7ba0bec4b0145ecd89
Josef8852 <josefaboshady2@gmail.com>
2026-03-08 00:03:11 +0100

    Setup ReactQuery & Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,25 @@ import Bookings from "./pages/Bookings";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime : 60*1000,
+    }
+  }
+});
 
 const App: React.FC = () => {
     
   return (
-    <>
-    <GlobalStyles/>
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles/>
+      <BrowserRouter>
         <Routes>
           <Route element={<AppLayout/>} >
             <Route index element={<Navigate replace to="/dashboard" />} />
@@ -27,12 +39,13 @@ const App: React.FC = () => {
             <Route path="/users" element={<NewUsers />} />
             <Route path="/bookings" element={<Bookings />} /> 
           </Route>
-          
+  
              <Route path="/login" element={<Login />} />  
              <Route path="*" element={<PageNotFound />} /> 
+          
         </Routes>
-      </BrowserRouter>
-    </>
+        </BrowserRouter>
+      </QueryClientProvider>
   )
   
 }
