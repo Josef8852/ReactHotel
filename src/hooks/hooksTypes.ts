4269133362646c00ff06275eebc9626917c64f7f
Josef8852 <josefaboshady2@gmail.com>
2026-03-13 07:47:34 +0100

    Typo

--- src/hooks/hooksTypes.ts
+++ src/hooks/hooksTypes.ts
@@ -4,7 +4,7 @@ import type { RefObject } from "react";
 export type Handler = () => void; 
 
 
-// Generic type by default its a div but we can pass any Html eleemnt to use the hook 
+// Generic type  ->  pass any Html element to use the hook 
 
 export type useClickType = <T extends HTMLElement>(
   handler: Handler
