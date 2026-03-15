d65a02356cfe5c5b8f2a616099ce337e2b74cd35
Josef8852 <josefaboshady2@gmail.com>
2026-03-15 02:30:17 +0100

    Improve filtering cabins

--- src/features/cabins/useFilterCabins.ts
+++ src/features/cabins/useFilterCabins.ts
@@ -3,20 +3,19 @@ import { type Cabin } from "./CabinTypes";
 
 const useFilterCabins = (value: string, cabinsToFilter: Cabin[]): Cabin[] => {
   
-  let filteredCabins : Cabin[] = []; 
+ 
   
-  if (value === "all" && cabinsToFilter) filteredCabins = cabinsToFilter;
   
   if (value === "no-discount" && cabinsToFilter) {
-    filteredCabins = cabinsToFilter?.filter((cabin) => cabin.discount === 0);
+    return cabinsToFilter?.filter((cabin) => cabin.discount === 0);
   }
   
   if (value === "with-discount" && cabinsToFilter) {
-    filteredCabins = cabinsToFilter?.filter((cabin) => cabin.discount !== 0);
+    return  cabinsToFilter?.filter((cabin) => cabin.discount !== 0);
   }
   
-  
-  return filteredCabins;
+  return cabinsToFilter;
+
 }
 
 export default useFilterCabins;
